45c650d208b4ac258c44fc18143a961f7e23510d
prussCoding <144256209+prussCoding@users.noreply.github.com>
2023-09-10 05:09:04 -0500

    updated ai enable example python file to support explanation and logging

--- Introduction-to-ai-in-networking/sections/ai-enabled-use-cases-in-networking/build-network-security/example.py
+++ Introduction-to-ai-in-networking/sections/ai-enabled-use-cases-in-networking/build-network-security/example.py
@@ -1,25 +1,40 @@
 import numpy as np
 from sklearn.ensemble import IsolationForest
 import time
+import logging
 
-# Simulated network traffic data (you would replace this with real data)
-# In this example, we generate random traffic values.
-network_traffic = np.random.normal(100, 20, 1000)
+# Set logging basic configuration
+logging.basicConfig(format='%(levelname)s:  %(message)s', level=logging.DEBUG)
 
-# Create an isolation forest model for anomaly detection
+# Set Normal Distribution Variables
+mean = 100
+standard_deviation = 20
+size = 1000
+
+# Set Baseline Traffic 
+network_traffic = np.random.normal(mean, standard_deviation, size)
+
+# Create an isolation forest model for anomaly detection and fit baseline traffic
 model = IsolationForest(contamination=0.05)  # 5% of data is considered anomalous
+model.fit(network_traffic.reshape(-1, 1))
+
 
 # Continuously monitor network traffic for anomalies
 while True:
     # Simulate real-time network traffic data (replace with actual data)
-    new_traffic = np.random.normal(100, 20, 1)
-
+    new_traffic = np.random.normal(mean, standard_deviation, 1)
+    
+    # Create standard message for logging
+    message = f'network activity detected: {new_traffic[0]}'
+ 
     # Predict if the new data point is anomalous
-    anomaly_score = model.fit_predict(new_traffic.reshape(-1, 1))
-
+    anomaly_score = model.predict(new_traffic.reshape(-1, 1))
+    
     if anomaly_score[0] == -1:
-        print(f"Suspicious network activity detected: {new_traffic[0]}")
+        logging.warning(f"Suspicious {message}")
         # Implement a response mechanism (e.g., block IP, alert admin) here
-
+    else: 
+        logging.info(f"Accepted {message}")
+    
     # Sleep for a short period before the next observation
     time.sleep(1)
--- Introduction-to-ai-in-networking/sections/ai-enabled-use-cases-in-networking/routing-algorithm/example.py
+++ Introduction-to-ai-in-networking/sections/ai-enabled-use-cases-in-networking/routing-algorithm/example.py
@@ -1,5 +1,8 @@
 import heapq
+import logging
 
+# Set logging basic configuration
+logging.basicConfig(format='%(levelname)s:  %(message)s - (Line no: %(lineno)d)', level=logging.DEBUG)
 class NetworkGraph:
     def __init__(self):
         self.graph = {}
@@ -16,27 +19,39 @@ class NetworkGraph:
     def dijkstra(self, start_node, end_node):
         # Initialize distance and visited dictionaries
         distances = {node: float('inf') for node in self.graph}
+        logging.info(f'Instantiating Distance Dictionay: {distances}')
         visited = {}
+        logging.info(f'Instantiating Visted Dictionay: {visited}')
         distances[start_node] = 0
+        logging.info(f'Setting Start Node to Distances: {distances}')
 
         # Priority queue to track nodes to visit
         priority_queue = [(0, start_node)]
+        logging.info(f'Setting Priority Queue: {priority_queue}')
 
         while priority_queue:
+            logging.info(f'Current Proirity Queue: {priority_queue}')
             current_distance, current_node = heapq.heappop(priority_queue)
+            logging.info(msg=f'Called Heapq func(Pop) return values: ({current_distance}, {current_node}) -> Priority Queue: {priority_queue}')
 
             if current_node in visited:
+                logging.info(f'Current node ({current_node}) was found in Visted Dictionary')
                 continue
 
             visited[current_node] = True
+            logging.info(f'Add Current Node ({current_node}) to Visited Dictionary: {visited}')
 
             for neighbor, weight in self.graph[current_node]:
                 distance = current_distance + weight
-
+                logging.info(f'Neighbor ({neighbor}) Distance Calculation: {current_distance} + {weight} = {distance}')
                 if distance < distances[neighbor]:
-                    distances[neighbor] = distance
+                    distances[neighbor] = distance  
+                    logging.info(f'Add Neigbhor ({neighbor}) to Distance Dictionary: {distances}') 
                     heapq.heappush(priority_queue, (distance, neighbor))
-
+                    logging.info(f'Called Heapq func(Push) with params: ({distance}, {neighbor}) -> Priority Queue: {priority_queue}')
+                    
+                    
+        logging.info(f'Returning Distance: {distances[end_node]}')   
         return distances[end_node]
 
 # Create a network graph
@@ -51,6 +66,8 @@ network.add_edge("C", "D", 3)
 network.add_edge("D", "E", 1)
 
 # Find the shortest path from node A to E using Dijkstra's algorithm
-shortest_distance = network.dijkstra("A", "E")
+start_node = "B"
+end_node = "D"
+shortest_distance = network.dijkstra(start_node=start_node, end_node=end_node)
 
-print(f"Shortest Distance from A to E: {shortest_distance}")
+logging.info(f"Shortest Distance from {start_node} to {end_node}: {shortest_distance}")
